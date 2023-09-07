e5af15987429da0366f477d6dc796bc0806146da
arijitdas13105 <arijitdas13105@gmail.com>
2023-09-07 15:24:38 +0530

    base url

--- client/src/files/config.js
+++ client/src/files/config.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000";
-// const BASE_URL = "https://92ed-14-139-240-244.ngrok-free.app";
+// const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://eggsquare.onrender.com";
 
 export default BASE_URL;
